3254970b64dec4cf82fc6422556ead17e2412aa5
Dusan Klinec <dusan.klinec@gmail.com>
2022-04-04 15:24:07 +0200

    style: run black on monero unit tests

--- core/tests/test_apps.monero.crypto.py
+++ core/tests/test_apps.monero.crypto.py
@@ -9,7 +9,6 @@ if not utils.BITCOIN_ONLY:
 
 @unittest.skipUnless(not utils.BITCOIN_ONLY, "altcoin")
 class TestMoneroCrypto(unittest.TestCase):
-
     def test_encoding(self):
         point = unhexlify(
             b"2486224797d05cae3cba4be043be2db0df381f3f19cfa113f86ab38e3d8d2bd0"
